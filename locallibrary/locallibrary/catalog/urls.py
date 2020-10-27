61c4030b71d4028bec92ca4f272249644b4bd42d
ShikiBot <71587406+ShikiBot@users.noreply.github.com>
2020-10-27 13:26:30 +0500

    forgot it

--- locallibrary/locallibrary/catalog/urls.py
+++ locallibrary/locallibrary/catalog/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index, name='index'),
-    url(r'^books/$', views.BookListView.as_view(), name='books'),
-    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
-    url(r'^authors/$', views.AuthorsListView.as_view(), name='authors'),
-    url(r'^authors/(?P<pk>\d+)$', views.AuthorsDetailView.as_view(), name='author-detail')
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorsListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorsDetailView.as_view(), name='author-detail')
 ]
