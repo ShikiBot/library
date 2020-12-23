cd313488d9bb49f129c62fa8f5c7960625905cb0
bot <xxx-yyy-2014@inbox.ru>
2020-12-23 15:15:41 +0500

    for, work

--- locallibrary/locallibrary/catalog/urls.py
+++ locallibrary/locallibrary/catalog/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorsListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorsDetailView.as_view(), name='author-detail'),
-    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed')
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    url(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian')
 ]
